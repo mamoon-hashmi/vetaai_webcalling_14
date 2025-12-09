ac071d1331c2296e4a8b33b2c44c48a5ad8060fc
Mamoon Ur Rasheed <mamoon.aiwork@gmail.com>
2025-12-09 16:08:03 +0500

    Update app.js

--- app.js
+++ app.js
@@ -262,7 +262,7 @@
       display: flex;
       align-items: center;
       gap: 0.5rem;
-      padding: 16px 20px;
+      padding: 3px 20px;
       background: white;
       border: 1px solid #e5e7eb;
       border-radius: 6px;
@@ -604,4 +604,5 @@
   muteBtn.onclick = mute;
   unmuteBtn.onclick = unmute;
 })();
+
 
